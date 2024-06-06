255dbe2f4d4b3b2a094a02cd46fbeed6d9678ad9
arun <arun@gmail.com>
2024-06-06 10:28:17 +0530

    task completed

---
+++ app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import login,register,dashboard,add_task,my_task,my_profile
+
+urlpatterns = [
+    path('login/',login,name='login'),
+    path('register/',register,name='register'),
+    path('task/',add_task,name='task'),
+    path('dashboard/',dashboard,name='dashboard'),
+    path('my_task',my_task,name='my_task'),
+    path('my_profile',my_profile,name='my_profile')
+]
+
